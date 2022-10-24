c3e0b6254a87b847c2e9e619fbd14250c2b2b0a0
HuyhuyhuyBN <116604127+HuyhuyhuyBN@users.noreply.github.com>
2022-10-25 06:43:06 +0700

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,10 +4,10 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "Huydt1234.aternos.me",
+    port: "24707",
     username: "Test",
-    version: false
+    version: 1.12.1
     })
     bot.on('login', function() {
       bot.chat('/reginster 123123123 123123123')
